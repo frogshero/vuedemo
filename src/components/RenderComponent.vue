87e92a7625fea04a954b3adf34a40a793a526e46
wangguofu@ym.com <wangguofu@ym.com>
2020-12-01 13:47:22 +0800

    jsx

---
+++ src/components/RenderComponent.vue
@@ -0,0 +1,17 @@
+
+<script>
+import Vue from 'vue'
+
+export default 
+Vue.component('RenderComponent',
+    {
+        render: function (h) {
+            return (
+            <H1>
+                <span>Hello</span> world!
+            </H1>
+            )
+        }
+    }       
+)
+</script>
